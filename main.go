3bc97f2edf508e3e243327eb2e79b86900d04ae0
maaverik <nithinjosetom@gmail.com>
2022-01-18 16:11:42 +0530

    Added gorilla mux router

--- main.go
+++ main.go
@@ -3,22 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		// w.Write([]byte("<h1>Hello world</h1>")) -> this is called internally by Fprintf()
-		fmt.Fprintf(w, "<h1>Hello world</h1>")
-	} else if r.URL.Path == "/page2" {
-		fmt.Fprintf(w, "<h1>Other page</h1>")
-	} else {
-		http.Error(w, "404: page not found", http.StatusNotFound)
-	}
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello world</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Contact page</h1>")
+}
+
+func pageNotFound(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>404: page not found</h1>")
 }
 
 // the name main is essential for this function and package to run
 func main() {
 	// matches all paths
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	h := http.HandlerFunc(pageNotFound)
+	r.NotFoundHandler = h
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
 }
