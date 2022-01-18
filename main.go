7dab513aedb33fedc074071c8dbff372421914a7
maaverik <nithinjosetom@gmail.com>
2022-01-18 15:17:53 +0530

    Routing with if-else

--- main.go
+++ main.go
@@ -6,10 +6,19 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello world</h1>")
+	if r.URL.Path == "/" {
+		// w.Write([]byte("<h1>Hello world</h1>")) -> this is called internally by Fprintf()
+		fmt.Fprintf(w, "<h1>Hello world</h1>")
+	} else if r.URL.Path == "/page2" {
+		fmt.Fprintf(w, "<h1>Other page</h1>")
+	} else {
+		http.Error(w, "404: page not found", http.StatusNotFound)
+	}
 }
 
+// the name main is essential for this function and package to run
 func main() {
+	// matches all paths
 	http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe(":3000", nil)
 }
