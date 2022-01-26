b44cb0ecf3a9f145b987f91274a6879374cabfe3
maaverik <nithinjosetom@gmail.com>
2022-01-27 02:16:32 +0530

    Added navbar and air for hot reloading

--- views/view.go
+++ views/view.go
@@ -8,7 +8,7 @@ type View struct {
 }
 
 func CreateView(layout string, files ...string) *View {
-	files = append(files, "views/layouts/footer.gohtml", "views/layouts/bootstrap.gohtml")
+	files = append(files, "views/layouts/footer.gohtml", "views/layouts/bootstrap.gohtml", "views/layouts/navbar.gohtml")
 	t, err := template.ParseFiles(files...)
 
 	if err != nil {
