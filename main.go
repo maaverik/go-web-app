51457104193402ab541657db9741cae5ac670f6c
maaverik <nithinjosetom@gmail.com>
2022-02-12 12:37:39 +0530

    Added sign up page and refactored code

--- main.go
+++ main.go
@@ -8,37 +8,46 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeView *views.View
-var contactView *views.View
+var (
+	homeView    *views.View
+	contactView *views.View
+	signupView  *views.View
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil)
-	if err != nil {
-		panic(err)
-	}
+	must(homeView.Render(w, nil))
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err := contactView.Template.ExecuteTemplate(w, contactView.Layout, nil)
+	must(contactView.Render(w, nil))
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(w, nil))
+}
+func pageNotFound(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>404: page not found</h1>")
+}
+
+func must(err error) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func pageNotFound(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>404: page not found</h1>")
 }
 
 // the name main is essential for this function and package to run
 func main() {
 	homeView = views.CreateView("bootstrap", "views/home.gohtml")
 	contactView = views.CreateView("bootstrap", "views/contact.gohtml")
+	signupView = views.CreateView("bootstrap", "views/signup.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/signup", signup)
 	h := http.HandlerFunc(pageNotFound)
 	r.NotFoundHandler = h
 
--- views/view.go
+++ views/view.go
@@ -1,14 +1,38 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"net/http"
+	"path/filepath"
+)
+
+const (
+	dir       = "views/layouts/"
+	extension = ".gohtml"
+)
 
 type View struct {
 	Template *template.Template
 	Layout   string
 }
 
+// execute the Template with extra data passed along to render the view to the writer
+func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
+}
+
+// returns slice of all filenames of template files in layout directory
+func getLayoutFiles() []string {
+	files, err := filepath.Glob(dir + "*" + extension)
+	if err != nil {
+		panic(err)
+	}
+	return files
+}
+
+// parses all layout templates and extra templates passed along and returns a View object
 func CreateView(layout string, files ...string) *View {
-	files = append(files, "views/layouts/footer.gohtml", "views/layouts/bootstrap.gohtml", "views/layouts/navbar.gohtml")
+	files = append(files, getLayoutFiles()...)
 	t, err := template.ParseFiles(files...)
 
 	if err != nil {
